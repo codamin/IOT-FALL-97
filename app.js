282462c86363631969d5bae66ce003e3164add3c
amin asadi <aminasadi329@gmail.com>
2019-01-03 09:27:13 +0330

    port changed

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(function(err, req, res, next) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = (process.env.PORT || 3000);
 }
 app.listen(port);
 module.exports = app;
